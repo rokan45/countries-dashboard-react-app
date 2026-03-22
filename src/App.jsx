1402d9455676fd4d5f35d8a2cbb56fda8bed8dcb
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-23 00:34:08 +0600

    data fetch form api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Suspense } from 'react';
 import './App.css'
+import Countries from './Component/Countries';
+
+// const fetchCountriesData=()=>{
+//   const res=fetch('https://openapi.programming-hero.com/api/all')
+//   return res.json();
+// }
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
 
 function App() {
-
+  // const countriesData=fetchCountriesData();
+  // console.log(countriesData);
 
   return (
     <>
-      <section>
-        <h1>Project Initialize</h1>
-      </section>
-
+      <Suspense fallback={<h1>Data Loading..</h1>}>
+        <Countries countriesPromise={countriesPromise} ></Countries>
+      </Suspense>
     </>
   )
 }
--- src/Component/countries.jsx
+++ src/Component/countries.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { use } from 'react';
 
-const countries = () => {
+const Countries = ({countriesPromise}) => {
+    const countriesArray=use(countriesPromise);
+    const countriesData=countriesArray.countries
+    console.log(countriesData);
     return (
         <div>
-            
+            <h2>Countries: {countriesData.length} </h2>
         </div>
     );
 };
 
-export default countries;
+export default Countries;
